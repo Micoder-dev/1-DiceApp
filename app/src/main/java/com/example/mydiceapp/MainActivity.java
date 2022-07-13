f7695bb531495f8c981635db0a60fc47a84ddb61
Micoder-dev <micoder.com@gmail.com>
2022-07-13 07:44:26 +0530

    btn setOnClickListener dice1 random img generation added

--- app/src/main/java/com/example/mydiceapp/MainActivity.java
+++ app/src/main/java/com/example/mydiceapp/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.mydiceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView diceImage1 = findViewById(R.id.imgDice1);
+        ImageView diceImage2 = findViewById(R.id.imgDice2);
+
+        int [] diceImages = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6
+        };
+
+        Button btnRoll = findViewById(R.id.btnRollTheDice);
+
+        btnRoll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random rndObject = new Random();
+
+                int myRandomNumber = rndObject.nextInt(6); // 0 .. 5
+
+                diceImage1.setImageResource(diceImages[myRandomNumber]);
+
+
+            }
+        });
+
     }
 }
