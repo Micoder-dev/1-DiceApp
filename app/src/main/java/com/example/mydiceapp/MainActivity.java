dbd58d95a8a3916b262bd653b790ffa595b0af36
Micoder-dev <micoder.com@gmail.com>
2022-07-13 08:07:44 +0530

    dice mp3 added onClick & shake anim added in d1,d2

--- app/src/main/java/com/example/mydiceapp/MainActivity.java
+++ app/src/main/java/com/example/mydiceapp/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.mydiceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 import java.util.Random;
 
@@ -30,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnRoll = findViewById(R.id.btnRollTheDice);
 
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.dice_sound);
+
         btnRoll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,8 +43,22 @@ public class MainActivity extends AppCompatActivity {
                 Random rndObject = new Random();
 
                 int myRandomNumber = rndObject.nextInt(6); // 0 .. 5
-
                 diceImage1.setImageResource(diceImages[myRandomNumber]);
+
+                myRandomNumber = rndObject.nextInt(6);
+                diceImage2.setImageResource(diceImages[myRandomNumber]);
+
+                YoYo.with(Techniques.Shake)
+                        .duration(500)
+                        .repeat(0)
+                        .playOn(diceImage1);
+
+                YoYo.with(Techniques.Shake)
+                        .duration(500)
+                        .repeat(0)
+                        .playOn(diceImage2);
+
+                mp.start();
 
 
             }
